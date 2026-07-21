754f336d353e104016b036944c2a564538b744c2
aigbokhaevboernest <129731110+aigbokhaevboernest@users.noreply.github.com>
2026-07-21 18:26:45 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
-
+import SplashScreen from "./components/SplashScreen";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -78,6 +78,7 @@ const App = () => (
         </AuthProvider>
       </BrowserRouter>
     </TooltipProvider>
+  </SplashScreen>
   </QueryClientProvider>
 );
 
