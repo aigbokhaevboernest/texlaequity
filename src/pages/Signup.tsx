59b45038fc576c177b094c6cb0c81866f4b6a6d2
aigbokhaevboernest <129731110+aigbokhaevboernest@users.noreply.github.com>
2026-07-21 22:48:01 +0100

    Update Signup.tsx

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -128,7 +128,7 @@ const Signup = () => {
       <div className="absolute -top-40 -right-40 w-[500px] h-[500px] blob opacity-40 pointer-events-none" />
       <div className="w-full max-w-md relative">
         <Link to="/" className="flex items-center justify-center mb-8">
-          <img src="/tesla-wordmark.png" alt="Tesla" className="h-8 w-auto" />
+          <img src="/tesla-wordmark.png" alt="Tesla" className="h-6 w-auto" />
         </Link>
 
         <div className="glass rounded-3xl p-8 shadow-elegant">
