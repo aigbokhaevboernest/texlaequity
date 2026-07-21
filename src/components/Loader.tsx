697db5be1f76660f917a84e6360ce94cd08adf1a
aigbokhaevboernest <129731110+aigbokhaevboernest@users.noreply.github.com>
2026-07-21 14:17:54 +0100

    Create Loader.tsx

---
+++ src/components/Loader.tsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+  return (
+    <div className="fixed inset-0 z-[100] flex items-center justify-center bg-background">
+      <div className="loader" />
+    </div>
+  );
+};
+
+export default Loader;
