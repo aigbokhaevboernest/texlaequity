de04a531b1813a0729452eb54d460fcb0242ef6a
aigbokhaevboernest <129731110+aigbokhaevboernest@users.noreply.github.com>
2026-07-22 09:04:52 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
+import PublicLayout from "./components/PublicLayout";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
-
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
